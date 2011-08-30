dcc7ae479b4b09256a2a4646b23d7f9113547652
Karol Kuczmarski <karol.kuczmarski@gmail.com>
2011-08-30 22:42:04 +0200

    Initial commit

---
+++ gaeworkers/__init__.py
@@ -0,0 +1,9 @@
+'''
+Library for executing potentially long tasks in App Engine's background,
+using task queue for code execution and memcache for preserving state.
+
+@author: Karol Kuczmarski "Xion"
+@contact: karol.kuczmarski@gmail.com
+@license: MIT
+@since: 2011-08-30
+'''
